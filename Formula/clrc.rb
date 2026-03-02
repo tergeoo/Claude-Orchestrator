65e22393acd48a002e6dbf9f531fafcadf10ea87
tergeo <georgijter-grigoranc@MacBook-Pro-zxc-2.local>
2026-03-02 16:48:46 +0500

    fix: update formula sha256 checksums for v1.2.0

--- Formula/clrc.rb
+++ Formula/clrc.rb
@@ -7,11 +7,11 @@ class Clrc < Formula
   on_macos do
     on_arm do
       url "https://github.com/tergeoo/clrc/releases/download/v#{version}/clrc-darwin-arm64"
-      sha256 "PLACEHOLDER_ARM64"
+      sha256 "5da0a1d04e27b1ad2946946e41da3299c6ba3c4e5abca8d1651c816c1b104cf3"
     end
     on_intel do
       url "https://github.com/tergeoo/clrc/releases/download/v#{version}/clrc-darwin-amd64"
-      sha256 "PLACEHOLDER_AMD64"
+      sha256 "33440495248b761853e70f5db814b11a0beb35ddf43da83aecce7685d1e4dbad"
     end
   end
 
